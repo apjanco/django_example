31d04d7fea739cff4acf0827a5dc5f4b5e3024c8
ajanco <apjanco@gmail.com>
2020-10-28 12:32:38 -0400

    first

---
+++ main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+import names 
+import requests
+# Create your views here.
+def home(request):
+    context = {}
+
+    context['name'] = names.get_full_name()
+    context['words'] = ', '.join([a['word'] for a in requests.get('http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5').json()])
+    context['quotes'] = requests.get('https://thesimpsonsquoteapi.glitch.me/quotes?count=10').json()
+
+    return render(request, 'index.html', context)
+
+def item(request, item):
+    context = {}
+    return render(request, 'index.html', context)
